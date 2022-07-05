7817f726fdca51f63994c2ec1dffcd466c43f34b
teamKweku <teamkweku@outlook.com>
2022-07-05 12:01:42 +0000

    Adding images to models and updating default MEDIA_ROOT

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -108,6 +108,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+# Default root for user image uploads
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
