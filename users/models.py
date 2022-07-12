531a3c2f49e234611d06d2b3158a6e54e3dffd50
teamKweku <teamkweku@outlook.com>
2022-07-12 22:47:23 +0000

    Setting up signals for post_save and post_delete

--- users/models.py
+++ users/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
 
 # Create your models here.
 class Profile(models.Model):
@@ -22,7 +25,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):
@@ -34,3 +37,6 @@ class Skill(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+
