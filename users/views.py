9b5aa0ce1db075da8991184ed49421cffcc87593
teamKweku <teamkweku@outlook.com>
2022-07-14 22:38:29 +0000

    User authentication and authorization, flush messages intro

--- users/views.py
+++ users/views.py
@@ -1,8 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
 from .models import Profile
 
 
 # Create your views here.
+def login_user(request):
+
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not exist')
+
+        # Authenticate function creates a session for the user
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'Username or password was incorrect')
+
+    context = {
+
+    }
+    return render(request, 'users/login_register.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'User was successfully logged out.')
+    return redirect('login')
+
+
 def profiles(request):
     profiles = Profile.objects.all()
     context = {
